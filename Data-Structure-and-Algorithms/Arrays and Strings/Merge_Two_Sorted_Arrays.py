adc2962297b837fe16df27adde6c31d7f3c064c3
nandati01 <nandati.shrestha@maine.edu>
2024-04-03 17:46:18 -0400

    cleaned up a little

--- Data-Structure-and-Algorithms/Arrays and Strings/Merge_Two_Sorted_Arrays.py
+++ Data-Structure-and-Algorithms/Arrays and Strings/Merge_Two_Sorted_Arrays.py
@@ -1,17 +1,7 @@
 # # Merge Two Sorted Arrays
-# 
-# Given two sorted integer arrays, return a new sorted array that combines the both of them. Example: arr1 = [1, 3, 5, 7], arr2 = [2, 4, 6, 8, 9], return [1, 2, 3, 4, 5, 6, 7, 8, 9]. 
-# 
-# To solve this problem, we can use the two pointers method. We will initially set the two pointers to the first index of the two arrays, respectively. The idea is to iteratively compare the elements that each pointer refers to, and selecting the smaller of the two to add to the new array.
-# 
-# Here's a solution breakdown:
-# - Initialize 'i' and 'j' at the beginning of arr1 and arr2 respectively
-# - Compare elements at i and j and insert the smaller of the two into a new array 'arr'
-# - Increment the pointer that pointed to the smaller array
-# - Once one of the pointers reaches the end of its array, append the remaining elements of the other array to arr
-# - Return the merged array
-# 
-# Since the arrays are already sorted, the time complexity for this algorithm is O(n + m)
+
+# Given two sorted integer arrays, return a new sorted array that combines the both of them. 
+# Example: arr1 = [1, 3, 5, 7], arr2 = [2, 4, 6, 8, 9], return [1, 2, 3, 4, 5, 6, 7, 8, 9]. 
 
 
 def combine(arr1, arr2):
