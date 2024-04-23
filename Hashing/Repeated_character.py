8b619623e202418792894edf3a592ca2cc63c4a1
nandati01 <nandati.shrestha@maine.edu>
2024-04-23 18:20:25 -0400

    some clarifications

--- Hashing/Repeated_character.py
+++ Hashing/Repeated_character.py
@@ -2,6 +2,7 @@ def repeated_characters(s):
     """
     Given a string s
     Return the first character to appear twice
+    The time complexity needs to be O(n)
     """
     seen = set()
     for c in s:
